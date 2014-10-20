00f5e8f583d3188c653e8fe6493fe88aaf216e63
郑文博 <zhengwenbo1995@gmail.com>
2014-10-20 20:56:52 +0800

    Update SY4_1.java

--- SY4_1.java
+++ SY4_1.java
@@ -15,32 +15,60 @@ public class SY4_1 {
 
 	  } 
 
-	  int max[]=new int[10]; 
-	  int ave[]=new int[10]; 
+	  int max1[]=new int[10]; 
+	  int max2[]=new int[10]; 
+	  int ave1[]=new int[10]; 
+	  int ave2[]=new int[10]; 
+	  for (int j=0;j<10;j++)
+	  {
+	   int temp1=arg[0][j]; 
+	   int sum1=0;
+	   for (int i=0;i<10;i++)
+	   { 
+	      if (arg[i][j]>temp1) 
+	     temp1=arg[i][j]; 
+	     sum1+=arg[i][j];
+	   }
+	   max1[j]=temp1; 
+       ave1[j]=sum1/10;
+	   }
 	  for (int i=0;i<10;i++)
-	  { 
-	   int temp=arg[i][0]; 
-	   int sum=0; 
+	  {
+	   int temp2=arg[i][0]; 
+	   int sum2=0;
 	   for (int j=0;j<10;j++)
 	   { 
-	    if (arg[i][j]>temp) 
-	     temp=arg[i][j]; 
-	     sum+=arg[i][j];     
-	   }    
-	   max[i]=temp; 
-       ave[i]=sum/10; 
-       } 
-  System.out.println("每行最大值分别为："); 
+	      if (arg[i][j]>temp2) 
+	     temp2=arg[i][j]; 
+	     sum2+=arg[i][j];
+	   }
+	   max2[i]=temp2; 
+       ave2[i]=sum2/10;
+	   }
+	  
+  System.out.println("每列最大值分别为："); 
   for (int i=0;i<10;i++)
   { 
-	   System.out.print(max[i]+"\t");    
+	   System.out.print(max1[i]+"\t");    
 	  } 
 	  System.out.print("\n"); 
-	  System.out.println("每行平均值分别为："); 
+	  System.out.println("每列平均值分别为："); 
 	  for (int i=0;i<10;i++)
 	  { 
-	   System.out.print(ave[i]+"\t");    
-	  } 
-	  System.out.print("\n"); 
-	 } 
+	   System.out.print(ave1[i]+"\t");    
+	  }
+	 System.out.print("\n"); 
+	System.out.println("每行最大值分别为："); 
+	 for (int i=0;i<10;i++)
+	 { 
+		   System.out.print(max2[i]+"\t");    
+		  } 
+		  System.out.print("\n"); 
+		  System.out.println("每行平均值分别为："); 
+		  for (int i=0;i<10;i++)
+		  { 
+		   System.out.print(ave2[i]+"\t");    
+		  } 
+		  System.out.print("\n"); 
+		}
 	} 
